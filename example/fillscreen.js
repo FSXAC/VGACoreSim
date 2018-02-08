c1b882fa4185319e569e9ab93f40356ec1e801b2
Muchen He <mansurhe@outlook.com>
2018-02-08 00:26:50 -0800

    Shortened the FS example

--- example/fillscreen.js
+++ example/fillscreen.js
@@ -1,14 +1,6 @@
 fsFunc = `for (var i = 0; i < 160; i++) {
     for (var j = 0; j < 120; j++) {
-        var n = i % 8;
-        if (n == 0) fill(0, 0, 0);
-        else if (n == 1) fill(0, 0, 255);
-        else if (n == 2) fill(0, 255, 0);
-        else if (n == 3) fill(0, 255, 255);
-        else if (n == 4) fill(255, 0, 0);
-        else if (n == 5) fill(255, 0, 255);
-        else if (n == 6) fill(255, 255, 0);
-        else if (n == 7) fill(255, 255, 255);
+        setColor(i % 8);
         pixel(i, j);
     }
 }`;
